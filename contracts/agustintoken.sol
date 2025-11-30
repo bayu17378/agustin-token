2bf7f2dd8008e6765800ec8debbd5d2a28ab729c
bayu17378 <arisriswara07@gmail.com>
2025-11-30 11:18:36 +0700

    Update agustintoken.sol
    Agustin Token (AG) adalah token ERC-20 sederhana yang dirancang sebagai contoh implementasi token digital menggunakan standar OpenZeppelin. Token ini dibuat dengan fokus pada kesederhanaan, keamanan, dan kompatibilitas penuh dengan ekosistem Ethereum Virtual Machine (EVM), sehingga dapat digunakan di jaringan seperti Ethereum, BNB Smart Chain, Polygon, Avalanche, dan lainnya.
    
    Token ini memiliki total suplai tetap sebesar 1.000.000 AG, yang seluruhnya dicetak (mint) ke alamat wallet deployer pada saat kontrak di-deploy. Dengan struktur kontrak yang minimalis dan standar industri, Agustin Token cocok dijadikan:
    
    Portfolio Web3 atau showcase kemampuan smart contract
    
    Token uji coba (test token) untuk belajar DeFi
    
    Demonstrasi pembuatan token ERC-20
    
    Proyek personal untuk latihan deploy di jaringan EVM
    
    
    Kontrak ini menggunakan library OpenZeppelin yang merupakan library paling populer dan dipercaya untuk pengembangan smart contract aman dan bebas dari kerentanan umum.

--- contracts/agustintoken.sol
+++ contracts/agustintoken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract AgustinToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("AgustinToken", "AGS") {
-        _mint(msg.sender, initialSupply * 10 ** decimals());
+    constructor() ERC20("Agustin Token", "AG") {
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
     }
 }
